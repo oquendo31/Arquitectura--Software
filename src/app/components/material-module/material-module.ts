439797fda22a583e04934f078cbce0078e3c666b
KlooNSizmicO <wilmardaniel@outlook.com>
2020-09-16 18:13:14 -0500

    Incorporado ng-material en loazy loading

---
+++ src/app/components/material-module/material-module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatTableModule} from '@angular/material/table';
+
+
+@NgModule({
+  declarations: [
+  ],
+  imports: [
+    CommonModule,
+    BrowserAnimationsModule,
+    MatTableModule
+  ],
+  exports: [MatTableModule]
+
+})
+export class MaterialModule { }
